3de5ab467dc253993dbeb1f2f6e8c99b423b5dad
George Kadianakis <desnacked@riseup.net>
2022-04-06 14:38:16 +0300

    Add fault injection feature (offline shufflers)

--- blind_and_swap/swapsim2.py
+++ blind_and_swap/swapsim2.py
@@ -8,7 +8,7 @@ DEFAULTS = {
     'rounds': 512,
     'swaps_per_round': 15,
     'log_level': 1,
-    'offline_percent': 0.0 # offline_percent of the validators will be offline and not shuffle
+    'offline_percent': 0 # offline_percent of the validators will be offline and not shuffle
 }
 
 def merge_probs(*probs):
@@ -42,7 +42,7 @@ def log(contents, level):
             contents = contents()
         print(contents)
 
-def run(width, rounds, swaps_per_round):
+def run(width, rounds, swaps_per_round, offline_percent):
     """
     Do a full run of the protocol
 
@@ -59,8 +59,15 @@ def run(width, rounds, swaps_per_round):
     log_swaps = len(bin(swaps_per_round + 1)) - 3
 
     for r in range(rounds):
-        log(" ------ ", 1)
-        log("Round {}".format(r), 1)
+        if r % 500  == 0:
+            log("Round {}".format(r), 1)
+            # log(" ------ ", 3)
+
+        # Do fault injection (offline shufflers)
+        if random.random() < float(offline_percent)/100: # offline 50% of the time
+            continue
+
+        # Iterate over each layer of the tree
         for depth in range(log_swaps):
             # Figure out index to shuffle and shuffle offset
             pivot = extract_positions[r - depth - 1]
@@ -88,10 +95,10 @@ def run(width, rounds, swaps_per_round):
 
     return output
 
-def test(width, rounds, swaps_per_round):
+def test(width, rounds, swaps_per_round, offline_percent):
     thresholds = []
 
-    outputs = run(width, rounds, swaps_per_round)
+    outputs = run(width, rounds, swaps_per_round, offline_percent)
 
     # Loop over each proposer
     for proposer_probs in outputs:
@@ -115,18 +122,18 @@ if __name__ == '__main__':
         if '=' in x:
             prefix, postfix = x[:x.index('=')], x[x.index('=')+1:]
             args[prefix] = int(postfix)
-        for arg, val in DEFAULTS.items():
-            if arg not in args:
-                print("Arg {} defaulted to {}".format(arg, val))
-                args[arg] = val
+    for arg, val in DEFAULTS.items():
+        if arg not in args:
+            print("Arg {} defaulted to {}".format(arg, val))
+            args[arg] = val
     LOG_LEVEL[0] = args['log_level']
 
     # Run the simulation
-    thresholds = test(args['width'], args['rounds'], args['swaps_per_round'])
+    thresholds = test(args['width'], args['rounds'], args['swaps_per_round'], args['offline_percent'])
 
-    for i in range(0, args['rounds'], 10):
-        print("After {} rounds, need to DoS {} validators for 20% chance of killing proposer".format(i, thresholds[i]))
+#    for i in range(0, args['rounds'], 10):
+#        print("After {} rounds, need to DoS {} validators for 20% chance of killing proposer".format(i, thresholds[i]))
     second_half = thresholds[len(thresholds)//2:]
-    print("Params [width: {}, rounds: {}, swaps: {}]".format(args['width'], args['rounds'], args['swaps_per_round']))
+    print("Params [width: {}, rounds: {}, swaps: {}, offline: {}]".format(args['width'], args['rounds'], args['swaps_per_round'], args['offline_percent']))
     print("Average 20%-diffusion of proposer: {}".format(sum(second_half) / len(second_half)))
     print("Frequency of 20%-diffusion = 1: {}".format(second_half.count(1) / len(second_half)))
